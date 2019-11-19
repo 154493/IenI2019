d99dae97fdd11bd4587c5a3e6b4174de8157da52
154493 <154493@csgaugustinus.nl>
2019-11-19 13:19:18 +0000

    bruh

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -1,6 +1,6 @@
 function setup() {
-  canvas = createCanvas(1000,500);
-  background('orange');
+  canvas = createCanvas(450,450);
+  background('gray');
   canvas.parent('processing');
   noLoop();
 }
@@ -9,13 +9,18 @@ function draw() {
   // groene cirkel zonder rand
   
   noStroke();
-  fill('green');
-  ellipse(300,200,400);
+  fill('blue');
+  ellipse(225,225,400);
   
+  noStroke();
+  fill('white');
+  ellipse(225,225,300);
   // witte rechthoek met rode rand
   
   stroke('red');
   fill('white');
   strokeWeight(10);
-  rect(650,100,250,300);  
+  rect(125,125,200,200);  
+
+
 }
--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -9,4 +9,9 @@ function draw() {
   noStroke();
   fill('darkred');
   triangle(225,0,0,450,450,450);
+
+  noStroke();
+  fill('pink');
+  triangle(0,450,450,450,450,450)
 }
+
